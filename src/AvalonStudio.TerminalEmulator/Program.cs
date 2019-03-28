f07766ad7c68c2d4ff7d93ccd7b50566c50fb86a
Dan Walmsley <dan@walms.co.uk>
2019-03-28 16:16:51 +0000

    add initial avalonia app for terminal emulator

---
+++ src/AvalonStudio.TerminalEmulator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+using AvalonStudio.TerminalEmulator.ViewModels;
+using AvalonStudio.TerminalEmulator.Views;
+
+namespace AvalonStudio.TerminalEmulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BuildAvaloniaApp().Start<MainWindow>(() => new MainWindowViewModel());
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .UseReactiveUI()
+                .LogToDebug();
+    }
+}
